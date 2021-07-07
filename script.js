748341e56ebefa9b061171faa0de95654c907ab5
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-07-07 12:18:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-getData()
+getData();
+
+async function getMovieGenres(){
+    try{
+        const data = await fetch (apiUrl);
+        const genres = await response.json();
+        movieGenres(genres.genres)
+        
+    } catch (err){
+        console.log('err')    
+    }}
+
+function movieGenres() {
+    itemlist = document.getElementById('genreMovies');
+    data.forEach(item => {
+        itemlist.innerHTML += `<li>Genre naam: ${item.name}, id: ${item.id}</li>`;
+    })}
+
+document.addEventListener("DOMContentLoaded", getMovieGenres());
