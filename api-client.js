7e234655af50d99ad8c0825c6c9b871e2fac4170
RandyLDP <80972485+RandyLDP@users.noreply.github.com>
2021-07-07 12:18:25 +0200

    Update api-client.js

--- api-client.js
+++ api-client.js
@@ -1,14 +1,13 @@
-const API_Key = '90fe6213ca00e7c19b576b393e7a44c1'
+const API_Key = '?api_key=90fe6213ca00e7c19b576b393e7a44c1';
 
 async function getData(){
     try{
-        const apiUrl = 'https://api.themoviedb.org/3/genre/movie/list'
-    
-        const res =  await fetch(apiUrl, {method:'GET'})
-        const processRes = await response.json()
+        const apiUrl = `https://api.themoviedb.org/3/genre/movie/list${API_KEY}`;
+        const res =  await fetch(apiUrl );
+        const processRes = await response.json();
+        console.log(getData);
         return processRes;
     } catch (error){
-        console.log('error')
-      }
- 
-}
+        console.log('error');
+      } }
+
